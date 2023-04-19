b43b2938d197a1c18cc72378d5e32ef5dad1d5ca
Reyannor <reyannorg41@gmail.com>
2023-04-19 00:24:06 -0700

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    colors: {
+      primary: '#eb144d',
+      secondary: '#23272a',
+      third: '#232323',
+      white: '#ffffff',
+      black: '#000000',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
